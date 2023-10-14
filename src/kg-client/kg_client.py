32d5c2ea0fe16b4acc021e263b10ec4340faed73
Guido Vetere <g.vetere@isagog.com>
2023-10-14 17:53:22 +0200

    refine kb interface

--- src/kg-client/kg_client.py
+++ src/kg-client/kg_client.py
@@ -11,120 +11,147 @@ import os
 
 from typing import Type, TypeVar
 
+from rdflib import RDFS
 
-from kg_model import Individual
+from kg_model import Individual, Entity, Assertion
 
-
-graph_endpoint = os.environ.get("ISAGOG_AI_KG_ROUTE", "https://ai.isagog.com/api/kg/graph")
 log = logging.getLogger("isagog-cli")
 
 E = TypeVar('E', bound='Entity')
 
 
-def fetch_entity(id: str,
-                 entity_type: Type[E] = Individual,
-                 limit=1024) -> E | None:
+class KnowledgeBase(object):
     """
-    Gets all individual entity data from the kg
-    :param id: the entity identifier
-    :param entity_type: the entity type
-    :param limit: limit
+    A KG proxy
     """
-    if id is None:
-        raise ValueError("Can't fetch None")
 
-    if not issubclass(entity_type, Individual):
-        raise ValueError(f"{entity_type} not an Entity")
+    def __init__(self, route: str, dataset: str):
+        """
 
-    res = requests.get(
-        url=graph_endpoint,
-        params=f"id={id}&expand=true&limit={str(limit)}",
-        headers={"Accept": "application/json"},
-    )
-    if res.ok:
-        log.debug("Fetched %s", id)
-        return entity_type(res.json())
-    else:
-        log.error("Couldn't fetch %s due to %s", id, res.reason)
-        return None
+        :param route: the service's endpoint route
+        :param dataset: the dataset name
+        """
+        assert (route and dataset)
+        self.endpoint = f"{route}/api/kg/graph"
+        self.dataset = dataset
 
+    def fetch_entity(self,
+                     id: str,
+                     entity_type: Type[E] = Entity,
+                     limit=None) -> E | None:
+        """
+        Gets all individual entity data from the kg
 
-def query_entity(id: str,
-                 properties: list[str],
-                 ) -> list[dict]:
-    """
-    Returns entity properties
-    :param id:
-    :param properties:
-    :return:
-    """
-    if id is None:
-        raise ValueError("Can't fetch None")
+        :param id: the entity identifier
+        :param entity_type: the entity type (default: Entity)
+        :param limit: limit of the number of assertions fetched (default: no limit)
+        """
 
-    if len(properties) == 0:
-        raise ValueError("Void entity query")
+        assert id
 
-    req = {
-        "subject": id,
-        "clauses": [{"property": prop, "project": True} for prop in properties]
-    }
+        if not issubclass(entity_type, Individual):
+            raise ValueError(f"{entity_type} not an Entity")
 
-    res = requests.post(
-        url=graph_endpoint,
-        json=req,
-        headers={"Accept": "application/json"},
-        timeout=30
-    )
+        params = f"id={id}&dataset={self.dataset}&expand=true"
 
-    if res.ok:
-        res_list = res.json()
-        if len(res_list) == 0:
-            log.error("void entity query")
-            return None
+        if limit:
+            params += f"&limit={str(limit)}"
+
+        res = requests.get(
+            url=self.endpoint,
+            params=params,
+            headers={"Accept": "application/json"},
+        )
+        if res.ok:
+            log.debug("Fetched %s", id)
+            return entity_type(res.json())
         else:
-            res_attrib_list = res_list[0].get('attributes', OSError("malformed response"))
+            log.error("Couldn't fetch %s due to %s", id, res.reason)
+            return None
 
-            def __get_attrib(prop: str) -> str:
-                try:
-                    record = next(item for item in res_attrib_list if item['id'] == prop)
-                    return record.get('values', OSError("malformed response"))
-                except StopIteration:
-                    raise OSError("incomplete response: %s not found", prop)
+    def query_assertions(self,
+                         subject_id: str,
+                         properties: list[str],
+                         dataset: str = None,
+                         ) -> list[Assertion]:  # todo: why is this a list and not a dict?
+        """
+        Returns entity properties
 
-            return [{prop: __get_attrib(f"<{prop}>")} for prop in properties]
-    else:
-        log.error("query of entity %s failed due to %s", id, res.reason)
-        return None
+        :param subject_id:
+        :param dataset: the dataset to fetch
+        :param properties: the queried properties
+        :return: a list of dictionaries { property: values }
+        """
+        assert (subject_id and properties)
 
-
-def search_named_entities(references: dict[str, str]) -> list[Individual]:
-    """
-    Retrieves named entities
-    :param references: a dictionary of "name" (key) : "type", where type is one of PER, LOC, ORG
-    :return:
-    """
-    entities = []
-    for name, kind in references.items():
         req = {
-            "kinds": [kind],
-            "clauses": [
-                {
-                    "property": "http://www.w3.org/2000/01/rdf-schema#label",
-                    "value": name,
-                    "method": "regex"
-                }
-            ]
+            "subject": subject_id,
+            "clauses": [{"property": prop, "project": True} for prop in properties]
         }
+
+        if dataset:
+            req["dataset"] = dataset
+
         res = requests.post(
-            url=graph_endpoint,
+            url=self.endpoint,
             json=req,
             headers={"Accept": "application/json"},
             timeout=30
         )
 
         if res.ok:
-            entities.extend([Individual(r) for r in res.json()])
+            res_list = res.json()
+            if len(res_list) == 0:
+                log.warning("void attribute query")
+                return []
+            else:
+                res_attrib_list = res_list[0].get('attributes', OSError("malformed response"))
 
-    return entities
+                def __get_attrib(prop: str) -> str:
+                    try:
+                        record = next(item for item in res_attrib_list if item['subject_id'] == prop)
+                        return record.get('values', OSError("malformed response"))
+                    except StopIteration:
+                        raise OSError("incomplete response: %s not found", prop)
 
+                return [Assertion(__get_attrib(f"<{prop}>")) for prop in properties] # todo this is gonna fail
+        else:
+            log.error("query of entity %s failed due to %s", subject_id, res.reason)
+            return None
 
+    def search_named_individuals(self,
+                                 references: dict[str, str],
+                                 dataset: str = None) -> list[Individual]:
+        """
+        Retrieves named entities
+        :param dataset:
+        :param references: a dictionary of "name" (key) : "type", where type is one of PER, LOC, ORG
+        :return:
+        """
+        entities = []
+        for name, kind in references.items():
+            req = {
+                "kinds": [kind],
+                "clauses": [
+                    {
+                        "property": "http://www.w3.org/2000/01/rdf-schema#label",
+                        "value": name,
+                        "method": "regex"
+                    }
+                ]
+            }
+
+            if dataset:
+                req["dataset"] = dataset
+
+            res = requests.post(
+                url=self.endpoint,
+                json=req,
+                headers={"Accept": "application/json"},
+                timeout=30
+            )
+
+            if res.ok:
+                entities.extend([Individual(r) for r in res.json()])
+
+        return entities
--- src/kg-client/kg_model.py
+++ src/kg-client/kg_model.py
@@ -4,6 +4,7 @@ Isagog KG model
 import logging
 from typing import Any
 
+from rdflib import OWL
 
 log = logging.getLogger("isagog-cli")
 
@@ -15,149 +16,6 @@ def _uri_label(uri: str) -> str:
         return uri.split("/")[-1]
     else:
         return uri
-
-
-class Attribute(object):
-    """
-    Attribute instance, as defined in
-    isagog_api/openapi/isagog_kg.openapi.yaml
-    """
-
-    def __init__(self, data: dict):
-        self.id = data.get('id', KeyError("attribute missing id"))
-        self.type = data.get('type', "string")
-        self.values = data.get('values', [])
-        self.label = data.get('label', _uri_label(self.id))
-        # self.comment = data['comment'] if 'comment' in data else ""
-
-    def to_dict(self) -> dict:
-        return _todict(self)
-
-    def all_values_as_string(self) -> str:
-        match len(self.values):
-            case 0:
-                return ""
-            case 1:
-                return self.values[0]
-            case _:
-                return "\n".join(self.values)
-
-    def all_values(self) -> list:
-        return self.values
-
-    def first_value(self, default=None) -> Any | None:
-        if len(self.values) > 0:
-            return self.values[0]
-        else:
-            return default
-
-    def is_empty(self) -> bool:
-        return len(self.values) == 0 or self.values[0] == "None"
-
-
-VoidAttribute = Attribute({'id': 'http://attribute#void'})
-
-
-class Reference(object):
-    """
-    Reference instance as defined in
-    isagog_api/openapi/isagog_kg.openapi.yaml
-    """
-
-    def __init__(self, data: dict):
-        self.id = data.get('id', KeyError("reference missing id"))
-        self.kinds = data.get('kinds', [])
-        self.label = data.get('label', _uri_label(self.id))
-
-    # self.comment = data['comment'] if 'comment' in data else ""
-
-    def to_dict(self) -> dict:
-        return _todict(self)
-
-
-class Relation(object):
-    """
-    Relation as defined in
-    isagog_api/openapi/isagog_kg.openapi.yaml
-    """
-
-    def __init__(self, data: dict):
-        try:
-            self.id = data['id']
-            self.label = data['label'] if 'label' in data and data['label'] != "" else _uri_label(self.id)
-            self.values = [Reference(r_data) for r_data in data['values']] if 'values' in data else []
-        except Exception as e:
-            raise ValueError(f"Deserialization error {e}")
-
-    def to_dict(self) -> dict:
-        return _todict(self)
-
-    def all_values(self) -> list:
-        return self.values
-
-    def first_value(self, default=None) -> Any | None:
-        if len(self.values) > 0:
-            return self.values[0]
-        else:
-            return default
-
-    def is_empty(self) -> bool:
-        return len(self.values) == 0
-
-
-VoidRelation = Relation({'id': 'http://relation#void'})
-
-
-class Individual(object):
-    """
-    Individual entity as defined in
-    isagog_api/openapi/isagog_kg.openapi.yaml
-    (Individual)
-    """
-
-    def __init__(self, data: dict):
-        try:
-            self.id = data['id']
-            self.kinds = data['kinds']
-            self.label = data['label'] if 'label' in data and data['label'] != "" else _uri_label(self.id)
-            self.comment = data['comment'] if 'comment' in data else ''
-            self.attributes = [Attribute(a_data) for a_data in data['attributes']] if 'attributes' in data \
-                else list[Attribute]()
-            self.relations = [Relation(r_data) for r_data in data['relations']] if 'relations' in data \
-                else list[Relation]()
-            self.score = float(data.get('score', 0.0))
-        except Exception as e:
-            raise ValueError(f"Deserialization error {e}")
-
-    def get_attribute(self, attribute_id: str) -> Attribute | Any:
-        # match_id = f"<{attribute_id}>" if not attribute_id.startswith('<') else attribute_id
-        found = next(filter(lambda x: x.id.strip('<>') == attribute_id, self.attributes), None)
-        if found and not found.is_empty():
-            return found
-        else:
-            log.warning("%s not valued in %s", attribute_id, self.id)
-            return VoidAttribute
-
-    def get_relation(self, relation_id: str) -> Relation | Any:
-        #   match_id = f"<{relation_id}>" if not relation_id.startswith('<') else relation_id
-        found = next(filter(lambda x: x.id.strip('<>') == relation_id, self.relations), None)
-        if found and not found.is_empty():
-            return found
-        else:
-            log.warning("%s not valued %s", relation_id, self.id)
-            return VoidRelation
-
-    def to_dict(self) -> dict:
-        return _todict(self)
-
-    def set_score(self, score: float):
-        self.score = score
-
-    def get_score(self) -> float | None:
-        return self.score
-
-    def has_score(self) -> bool:
-        return self.score is not None
 
 
 def _todict(obj, classkey=None):
@@ -182,3 +40,161 @@ def _todict(obj, classkey=None):
         return data
     else:
         return obj
+
+
+class Entity(object):
+    """
+    Any identified knowledge entity, either predicative or individual
+    """
+    ALLOWED_TYPES = [OWL.Axiom, OWL.NamedIndividual, OWL.ObjectProperty, OWL.DatatypeProperty]
+
+    def __init__(self, _id: str, _type: str):
+        assert (_id and _type)
+        assert _type in Entity.ALLOWED_TYPES
+        self.id = _id
+        self.type = _type
+
+    def to_dict(self) -> dict:
+        return _todict(self)
+
+
+class Assertion(object):
+    """
+    Any assertion
+    """
+
+    def __init__(self, predicate: str, subject='@', values=None):
+        assert predicate
+        self.predicate = predicate
+        self.subject = subject
+        self.values = values if values else []
+
+
+class AttributeInstance(Assertion):
+    """
+    Attribute instance, as defined in
+    isagog_api/openapi/isagog_kg.openapi.yaml
+    """
+
+    def __init__(self, data: dict):
+        super().__init__(predicate=data.get('id', KeyError("missing attribute id")),
+                         values=data.get('values', []))
+        self.value_type = data.get('type', "string")
+
+    def all_values_as_string(self) -> str:
+        match len(self.values):
+            case 0:
+                return ""
+            case 1:
+                return self.values[0]
+            case _:
+                return "\n".join(self.values)
+
+    def all_values(self) -> list:
+        return self.values
+
+    def first_value(self, default=None) -> Any | None:
+        if len(self.values) > 0:
+            return self.values[0]
+        else:
+            return default
+
+    def is_empty(self) -> bool:
+        return len(self.values) == 0 or self.values[0] == "None"
+
+
+VOID_ATTRIBUTE = AttributeInstance({'id': 'http://isagog.com/attribute#void'})
+
+
+class Concept(Entity):
+    """
+    Concept
+    isagog_api/openapi/isagog_kg.openapi.yaml
+    """
+
+    def __init__(self, data: dict):
+        super().__init__(data.get('id'), OWL.Class)
+        self.comment = data.get('comment', "")
+        self.ontology = data.get('ontology', "")
+        self.parents = data.get('parents', [])
+
+
+class Reference(Entity):
+    """
+    Reference instance as defined in
+    isagog_api/openapi/isagog_kg.openapi.yaml
+    """
+
+    def __init__(self, data: dict):
+        super().__init__(data.get('id'), OWL.Axiom)
+        self.kinds = data.get('kinds', [])
+
+
+class Relationship(Assertion):
+    """
+    Relation instance as defined in
+    isagog_api/openapi/isagog_kg.openapi.yaml
+    """
+
+    def __init__(self, data: dict, predicate: str):
+        super().__init__(predicate=data.get('id', KeyError("missing relation id")),
+                         values=[Reference(r_data) for r_data in data['values']] if 'values' in data else [])
+
+    def all_values(self) -> list:
+        return self.values
+
+    def first_value(self, default=None) -> Any | None:
+        if len(self.values) > 0:
+            return self.values[0]
+        else:
+            return default
+
+    def is_empty(self) -> bool:
+        return len(self.values) == 0
+
+
+VOID_RELATION = Relationship({'id': 'http://isagog.com/relation#void'})
+
+
+class Individual(Entity):
+    """
+    Individual entity as defined in
+    isagog_api/openapi/isagog_kg.openapi.yaml
+    (Individual)
+    """
+
+    def __init__(self, data: dict):
+        super().__init__(data.get('id'), OWL.NamedIndividual)
+        self.label = data.get('label', _uri_label(self.id))
+        self.kinds = data.get('kinds', [OWL.Thing])
+        self.comment = data.get('comment', '')
+        self.attributes = [AttributeInstance(a_data) for a_data in data['attributes']] if 'attributes' in data \
+            else list[AttributeInstance]()
+        self.relations = [Relationship(r_data) for r_data in data['relations']] if 'relations' in data \
+            else list[Relationship]()
+        self.score = float(data.get('score', 0.0))
+
+    def get_attribute(self, attribute_id: str) -> AttributeInstance | Any:
+        found = next(filter(lambda x: x.id.strip('<>') == attribute_id, self.attributes), None)
+        if found and not found.is_empty():
+            return found
+        else:
+            log.warning("%s not valued in %s", attribute_id, self.id)
+            return VOID_ATTRIBUTE
+
+    def get_relation(self, relation_id: str) -> Relationship | Any:
+        found = next(filter(lambda x: x.id.strip('<>') == relation_id, self.relations), None)
+        if found and not found.is_empty():
+            return found
+        else:
+            log.warning("%s not valued %s", relation_id, self.id)
+            return VOID_RELATION
+
+    def set_score(self, score: float):
+        self.score = score
+
+    def get_score(self) -> float | None:
+        return self.score
+
+    def has_score(self) -> bool:
+        return self.score is not None
